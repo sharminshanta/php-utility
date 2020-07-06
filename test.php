74a9f480bbad804628fd89d464cb92b1ca5ebef0
Sharmin Shanta <mimetyshanta@gmail.com>
2020-07-06 20:24:21 +0600

    Delete test.php

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Sharminshanta\Web\PHPUtilities\Utility;
-
-require_once "vendor/autoload.php";
-
-var_dump(Utility::generateUUID());
-var_dump(Utility::generateSlugify("Sharmin Shanta"));
-die();
